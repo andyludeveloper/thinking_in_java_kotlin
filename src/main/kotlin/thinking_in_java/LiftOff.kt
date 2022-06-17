682963df8a90d13d343559accb1184e08e6567b0
Andy <andyludeveloper@gmail.com>
2022-06-17 17:53:06 +0800

    [Example1] LiftOff

---
+++ src/main/kotlin/thinking_in_java/LiftOff.kt
@@ -0,0 +1,27 @@
+package thinking_in_java
+
+class LiftOff(private var countDown: Int = 10) : Runnable {
+    private val id = taskCount++
+
+    companion object {
+        var taskCount = 0
+    }
+
+    private fun status(): String = if (countDown > 0) {
+        "#$id($countDown),"
+    } else {
+        "#$id(Liftoff!),"
+    }
+
+    override fun run() {
+        while (countDown-- > 0) {
+            println(status())
+            Thread.yield()
+        }
+    }
+}
+
+fun main() {
+    val launch = LiftOff()
+    launch.run()
+}
